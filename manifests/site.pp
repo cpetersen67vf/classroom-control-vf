d00d18cb8873908e183856cd98e98a6cd1df66f1
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-28 14:25:19 -0500

    lab 7.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname} and i am not a huge fan of all the git stuff": }
+  file { '/etc/motd':
+      ensure => file,
+      owner => 'root',
+      group => 'root',
+      mode => '0644',
+      content => 'Puppet code is fun to write!',
+  }
 }
