6dfa8c8008b7ac52095e3f1b029580f1e778401a
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-29 12:45:46 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,17 @@
-
+class memcached {
+    package { 'memcached':
+        ensure => present,
+    }
+    
+    file { '/etc/sysconfig/memcached':
+        ensure => present,
+        source => 'puppet:///modules/memcached/memcached',
+        require => Package['memcached'],
+    }
+    
+    service { 'memcached':
+        ensure => running,
+        enable => true,
+        subscribe => File['/etc/sysconfig/memcached'],
+    }
+}
