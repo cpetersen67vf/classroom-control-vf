a3e521155f9bdb459852c9d74cdd5bcda4996f2d
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-29 09:46:29 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,15 @@
-
+class skeleton {
+    file { '/etc/skel':
+        ensure => directory,
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+    }
+    file { '/etc/skel/.bashrc':
+        ensure => present,
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+        source => 'puppet:///modules/skeleton/bashrc',
+    }
+}
