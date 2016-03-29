92eb6f5721e7c4843d464129599956ee2d58aa31
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-29 13:57:04 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,34 @@
-
+class nginx {
+    package { 'nginx':
+        ensure => present,
+    }
+    
+    file { '/etc/nginx/nginx.conf':
+        ensure => present,
+        source => 'puppet:///modules/nginx/nginx.conf',
+        require => Package['nginx'],
+    }
+    
+    file { '/etc/nginx/conf.d/default.conf':
+        ensure => present,
+        source = 'puppet:///modules/nginx/default.conf',
+        require => Package['nginx'],
+    }
+    
+    file { '/var/www':
+        ensure => directory,
+        require => Package['nginx'],
+    }
+    
+    file { '/var/www/index.html':
+        ensure => present,
+        require => File['/var/www'],
+        source => 'puppet:///modules/nginx/index.html',
+    }
+    
+    service { 'nginx':
+        ensure => running,
+        enable => true,
+        subscribe => File['/var/www/index.html'],
+    }
+}
