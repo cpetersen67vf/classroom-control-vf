a0e8b6dfc7d620575f329bfa0d605089657bf525
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-30 13:22:45 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,1 +1,29 @@
-
+define users::managed_user ( home = "/home/${title}", group, shell = '/bin/bash', authkey, ) {
+    
+    name = $title
+    
+    File {
+        owner => $name,
+        group => $group,
+        mode => '0700',
+    }
+    user { $name:
+        ensure => present,
+        gid => $group,
+        home = $home,
+        shell = $shell,
+    }
+    file { $home:
+        ensure => directory,
+    }
+    file { "${home}/.ssh":
+        ensure => directory,
+    }
+    if $authkey {
+        ssh_authorized_key { "${name} default key":
+            user => $name,
+            type => 'ssh-rsa',
+            key => $authkey,
+        }
+    }
+}
