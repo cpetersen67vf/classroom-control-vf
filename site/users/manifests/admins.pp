9ac016c25c643a5522a7c97047fcdea8cfcab6d7
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-30 13:26:08 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,1 +1,15 @@
+class admins {
 
+    users::managed_user { 'jose':
+        group => 'users',
+    }
+    
+    users::managed_user { 'alice':
+        group => 'users',
+    }
+    
+    users::managed_user { 'chen':
+        group => 'users',
+    }
+
+}
