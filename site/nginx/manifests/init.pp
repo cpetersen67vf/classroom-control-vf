5c75b273b82b5fd4f259e2be957d1e35641fe33f
cpetersen67vf <cpetersen@crystallized-software.com>
2016-03-30 09:13:20 -0500

    match the lab better

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,7 +1,8 @@
 class nginx {
     File {
-        owner => 'nginx',
-        group => 'nginx',
+        ensure => file,
+        owner => 'root',
+        group => 'root',
         mode => '0644',
     }
     
@@ -10,13 +11,11 @@ class nginx {
     }
     
     file { '/etc/nginx/nginx.conf':
-        ensure => present,
         source => 'puppet:///modules/nginx/nginx.conf',
         require => Package['nginx'],
     }
     
     file { '/etc/nginx/conf.d/default.conf':
-        ensure => present,
         source => 'puppet:///modules/nginx/default.conf',
         require => Package['nginx'],
     }
@@ -27,7 +26,6 @@ class nginx {
     }
     
     file { '/var/www/index.html':
-        ensure => present,
         require => File['/var/www'],
         source => 'puppet:///modules/nginx/index.html',
     }
